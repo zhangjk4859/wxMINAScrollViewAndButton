e4efef751128f190cd961f633a80044e476a5a64
zhangjk4859 <573581325@qq.com>
2016-11-02 15:13:47 +0800

    细节补充

--- pages/example/example.js
+++ pages/example/example.js
@@ -1,18 +1,18 @@
 Page({
   data:{
-    //按钮设置
-    titleColor : 'rgb(50,170,50)',        //标题按钮背景颜色
-    titleHeight:100,                      //标题高度，单位为rpx
-    fontColor:'black',                    //未选中字体颜色
-    selectedFontColor : 'red',          //已选中字体颜色
-    //指示器
-    indicatorColor : 'red',            //指示条颜色
-    bgColor : 'white',                    //指示条背景颜色
-    indicatorHeight:10,                   //指示器高度，单位为rpx
+    //按钮设置参数
+    titleColor : 'rgb(50,170,50)',  //标题按钮背景颜色
+    titleHeight:100,                //标题高度，单位为rpx
+    fontColor:'black',              //未选中字体颜色
+    selectedFontColor : 'red',      //已选中字体颜色
+    //指示器设置参数
+    indicatorColor : 'red',         //指示条颜色
+    bgColor : 'white',              //指示条背景颜色
+    indicatorHeight:10,             //指示器高度，单位为rpx
     
-    fontSizeL:15,                         //按钮字体，单位px,默认15
-    fontSizeS:10,                         //按钮字体，单位px,标题数大于5自动缩小为13
-    thresholdCount : 5,                   //字体变化的数组数量临界点，默认为5组
+    fontSizeL:15,                   //按钮字体，单位px,默认15
+    fontSizeS:13,                   //按钮字体，单位px,标题数大于5自动缩小为13
+    thresholdCount : 5,             //字体变化的数组数量临界点，默认为5组
 
     //在这个数组中按照格式增加和减少标题的数量               
     titles:[
@@ -40,8 +40,7 @@ Page({
       this.computeWidth();
       //初始化指示器，默认第一个按钮选中
       this.defaultIndicator(0);
-
-      //大于五个标题，字体设置为13号进行自适应
+      //大于一定数量标题，字体设置为13号（默认）进行自适应
       var count = this.data.titles.length;
       var thresholdCount = this.data.titles.thresholdCount;
       if(count > thresholdCount){
